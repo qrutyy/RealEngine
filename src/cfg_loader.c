b9eb659aa49386ca79edbbb19e66f204fab2eea7
Mikhail Gavrilenko <64466788+qrutyy@users.noreply.github.com>
2025-10-31 20:50:25 -0400

    feat: data-driven level (cfg) load and game state serialization #5
    * draft: add initial cfg structure + example
    
    * feat: implement cfg parser and loader
    
    * fix: remove wrong include
    
    * fix: entity init type in loader
    
    * fix: assign layers + minors
    
    * feat: add cfg guide + fix the map dim
    
    * refactor: switch to eng + log_prints

---
+++ src/cfg_loader.c
@@ -0,0 +1,79 @@
+#include "asset.h"
+#include "entity.h"
+#include "include.h"
+#include "scene.h"
+#include "log.h"
+#include "cfg_parser.h"
+
+asset_cfg_t *find_asset_by_shortcut(map_layout_cfg_t *config, const char shortcut) {
+	for (int i = 0; i < MAX_ASSETS; i ++) {
+		if (config->assets[i].shortcut == shortcut)
+			return &config->assets[i];
+	}
+	return NULL;
+}
+
+entity_cfg_t *get_entity_with_type(map_layout_cfg_t *config, const char type_shortcut) {
+	for (int i = 0; i < config->entity_count; i ++) {
+		if (config->entities[i].shortcut == type_shortcut) {
+			return &config->entities[i];
+		}	
+	}
+	return NULL;
+}
+void assign_layers(grid_t *grid, map_layout_cfg_t *config) {
+	int parsed_entity_count = 0;
+	asset_cfg_t *asset;
+	entity_cfg_t *curr_entity_cfg;
+
+	for (int y = 0; y < config->grid_height; y++) {
+		for (int x = 0; x < config->grid_width; x++) {
+			char shortcut = config->layout[0][y][x];
+			if (shortcut == '\0') continue;
+
+			asset = find_asset_by_shortcut(config, shortcut);
+
+			if (asset) {
+				RE_assign_asset_static(grid, asset->engine_asset_key, 0, x, y);
+			}
+		}
+	}
+
+	for (int i = 1; i < config->layer_count; i++) {
+		for (int y = 0; y < config->grid_height; y++) {
+			for (int x = 0; x < config->grid_width; x++) {
+				char shortcut = config->layout[i][y][x];
+
+				if (shortcut != '-' && shortcut != '\0') {
+					curr_entity_cfg = get_entity_with_type(config, shortcut);
+
+					if (curr_entity_cfg) {
+						RE_add_entity(x, y, curr_entity_cfg->type);
+						parsed_entity_count++;
+					}
+				}
+			}
+		}
+	}
+
+	if (config->entity_count != parsed_entity_count) {
+		log_error("Warning: Number of entities in config (%d) does not match entities found in layout (%d)\n", config->entity_count, parsed_entity_count);
+	}
+}
+
+int load_cfg(grid_t *grid, map_layout_cfg_t *config) {
+	int i;
+	asset_cfg_t *curr_asset_cfg;
+
+	RE_init_grid(grid, config->grid_width, config->grid_height, config->tile_width, config->tile_height);
+
+	for (i = 0; i < config->asset_count; i ++) {
+		curr_asset_cfg = &config->assets[i];
+		curr_asset_cfg->engine_asset_key = RE_load_asset(curr_asset_cfg->filename, curr_asset_cfg->pos_x, curr_asset_cfg->pos_y, curr_asset_cfg->dim_w, curr_asset_cfg->dim_h);
+	}
+
+	assign_layers(grid, config);
+//	assign_animations(); TODO: in parser as well
+}
+
+
