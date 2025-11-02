108ccbf5228c17051e52cf5956574a10d2e7c422
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2025-11-02 14:54:30 +0300

    test: add tests for asset.c

--- tests/test_runner.c
+++ tests/test_runner.c
@@ -2,9 +2,11 @@
 #include "unity_internals.h"
 
 extern void run_app_tests(void);
+extern void run_asset_tests(void);
 
 int main(void) {
     UNITY_BEGIN();
     run_app_tests();
+    run_asset_tests();
     return UNITY_END();
 }
